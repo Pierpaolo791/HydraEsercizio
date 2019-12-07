a7ae988ad79f4569d68487b5ac6155cc7360a5ca
Pierpaolo791 <pierpaolo.pecoraio@gmail.com>
2019-12-07 12:33:41 +0100

    add scenariohydra

---
+++ src/hydra/hydra/ScenarioHydra.java
@@ -0,0 +1,14 @@
+package hydra.hydra;
+
+public class ScenarioHydra {
+    
+    private final Hydra hydra;
+
+    public ScenarioHydra(Hydra hydra) {
+        this.hydra = hydra;
+    }
+    
+    public void executeAttacco() {
+        
+    }
+}
